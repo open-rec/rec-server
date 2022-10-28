e811dbf008197a4cb190de023a6a1699baf28a62
xsank <xsank@foxmail.com>
2022-10-28 16:54:00 +0800

    add request id

--- proto/src/main/java/com/openrec/proto/JsonReq.java
+++ proto/src/main/java/com/openrec/proto/JsonReq.java
@@ -3,8 +3,18 @@ package com.openrec.proto;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class JsonReq<T> {
     protected String requestId;
     protected T body;
+
+    public JsonReq() {
+        this(null);
+    }
+    public JsonReq(T body) {
+        this.requestId = UUID.randomUUID().toString();
+        this.body = body;
+    }
 }
