f5538268402d563e08a92cd1467f0d3cef3880cf
xsank <xsank@foxmail.com>
2022-11-10 14:51:41 +0800

    fix es client pop

--- server/src/main/java/com/openrec/service/es/EsService.java
+++ server/src/main/java/com/openrec/service/es/EsService.java
@@ -17,34 +17,34 @@ import java.util.List;
 public class EsService {
 
     @Autowired
-    private ElasticsearchClient esClient;
+    private ElasticsearchClient elasticsearchClient;
 
 
     public void createIndex(String indexName, String indexDef) throws IOException {
         CreateIndexRequest request = CreateIndexRequest
                 .of(i -> i.index(indexName).withJson(new StringReader(indexDef)));
-        esClient.indices().create(request).acknowledged();
+        elasticsearchClient.indices().create(request).acknowledged();
     }
 
     public void deleteIndex(String indexName) throws IOException {
         DeleteIndexRequest request = DeleteIndexRequest
-                .of(i->i.index(indexName));
-        esClient.indices().delete(request);
+                .of(i -> i.index(indexName));
+        elasticsearchClient.indices().delete(request);
     }
 
     public void bulk(String indexName, List<Pair<Integer, Object>> docs) throws IOException {
         BulkRequest.Builder bulkReqBuilder = new BulkRequest.Builder();
-        for(Pair<Integer,Object> doc : docs){
-            bulkReqBuilder.operations(op->op.index(i->i.index(indexName)
+        for (Pair<Integer, Object> doc : docs) {
+            bulkReqBuilder.operations(op -> op.index(i -> i.index(indexName)
                     .id(String.valueOf(doc.getLeft()))
                     .document(doc.getRight())));
         }
-        esClient.bulk(bulkReqBuilder.build());
+        elasticsearchClient.bulk(bulkReqBuilder.build());
     }
 
     public void search(String indexName, String query) throws IOException {
         SearchRequest request = SearchRequest
-                .of(i->i.index(indexName).withJson(new StringReader(query)));
-        esClient.search(request, Void.class);
+                .of(i -> i.index(indexName).withJson(new StringReader(query)));
+        elasticsearchClient.search(request, Void.class);
     }
 }
