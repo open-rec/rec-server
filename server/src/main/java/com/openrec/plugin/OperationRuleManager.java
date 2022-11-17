a4934aea5ae338f32d130d8acdf4dd6a1c60c7d8
xsank <xsank@foxmail.com>
2022-11-17 19:58:41 +0800

    avoid load jar failed

--- server/src/main/java/com/openrec/plugin/OperationRuleManager.java
+++ server/src/main/java/com/openrec/plugin/OperationRuleManager.java
@@ -5,15 +5,18 @@ import java.nio.file.Paths;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 import org.pf4j.JarPluginManager;
 import org.pf4j.PluginManager;
 
 import com.openrec.contrib.operation.OperationRule;
 
+@Slf4j
 public class OperationRuleManager {
 
     private static final String JAR_FILE =
-        System.getProperty("user.dir") + File.separator + "/plugins/rec-contrib-1.0-SNAPSHOT.jar";
+        System.getProperty("user.dir") + File.separator + "plugins/rec-contrib-1.0-SNAPSHOT.jar";
     private static final String PLUGIN_ID = "contrib-plugins";
 
     private static PluginManager pluginManager;
@@ -21,10 +24,15 @@ public class OperationRuleManager {
 
     static {
         pluginManager = new JarPluginManager();
-        load(JAR_FILE, PLUGIN_ID);
+        operationRuleMap = Maps.newHashMap();
+        try {
+            load(JAR_FILE, PLUGIN_ID);
+        } catch (Exception e) {
+            log.error("load jar file:{} failed, plugin:{}", JAR_FILE, PLUGIN_ID);
+        }
     }
 
-    public static void load(String jarFile, String pluginId) {
+    public static void load(String jarFile, String pluginId) throws Exception{
         pluginManager.loadPlugin(Paths.get(jarFile));
         pluginManager.startPlugin(pluginId);
         operationRuleMap = pluginManager.getExtensions(OperationRule.class).stream()
